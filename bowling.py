daa41be9b48d7824e152b6b30a06f9c19e56fd87
Orsolya Lévai <solya0082@gmail.com>
2017-11-21 19:36:01 +0100

    Reformatting the conditionals in the score function with existing bool functions.

--- bowling.py
+++ bowling.py
@@ -2,31 +2,47 @@ def score(game):
     result = 0
     frame = 1
     in_first_half = True
+
     for i in range(len(game)):
-        if game[i] == '/':
+        if is_a_spare(game[i]):
             result += 10 - last
         else:
             result += get_value(game[i])
+
         if frame < 10 and get_value(game[i]) == 10:
-            if game[i] == '/':
+            if is_a_spare(game[i]):
                 result += get_value(game[i+1])
-            elif game[i] == 'X' or game[i] == 'x':
+            elif is_a_strike(game[i]):
                 result += get_value(game[i+1])
-                if game[i+2] == '/':
+                if is_a_spare(game[i+2]):
                     result += 10 - get_value(game[i+1])
                 else:
                     result += get_value(game[i+2])
         last = get_value(game[i])
+
         if not in_first_half:
             frame += 1
         if in_first_half:
             in_first_half = False
         else:
             in_first_half = True
-        if game[i] == 'X' or game[i] == 'x':
+        if is_a_strike(game[i]):
             in_first_half = True
             frame += 1
     return result
+
+
+def get_value(char):
+    if is_a_score_between_one_and_nine(char):
+        return int(char)
+    elif is_a_strike(char):
+        return 10
+    elif is_a_spare(char):
+        return 10
+    elif is_a_miss(char):
+        return 0
+    else:
+        raise ValueError()
 
 
 def is_a_score_between_one_and_nine(char):
@@ -45,16 +61,3 @@ def is_a_spare(char):
 
 def is_a_miss(char):
     return char == '-'
-
-
-def get_value(char):
-    if is_a_score_between_one_and_nine(char):
-        return int(char)
-    elif is_a_strike(char):
-        return 10
-    elif is_a_spare(char):
-        return 10
-    elif is_a_miss(char):
-        return 0
-    else:
-        raise ValueError()
