15e25ecab872e80e586dd9f2b82c79165891778d
Orsolya Lévai <solya0082@gmail.com>
2017-11-21 21:37:30 +0100

    Made all_pins_down_in_the_first_roll() for better understand the situation, when all pins down at the first roll

--- bowling.py
+++ bowling.py
@@ -10,48 +10,17 @@ def scoring_when_all_pins_down(game, result, frame, total_frames, max_pins_numbe
     return result
 
 
-def score(game):
-    result = 0
-    frame = 1
-    in_first_half = True
-    total_frames = 10
-    max_pins_number = 10
-
-    for roll_index in range(len(game)):
-        if is_a_spare(game[roll_index]):
-            result += total_frames - last_frame
-        else:
-            result += get_value(game[roll_index])
-
-        if frame < total_frames and get_value(game[roll_index]) == max_pins_number:
-            result = scoring_when_all_pins_down(game, result, frame, total_frames, max_pins_number, roll_index)
-        
-        last_frame = get_value(game[roll_index])
-
-        if in_first_half:
-            in_first_half = False
-        else:
-            frame += 1
-            in_first_half = True
-
-        if is_a_strike(game[roll_index]):
-            in_first_half = True
-            frame += 1
-
-    return result
-
-
-def get_value(char):
-    if is_a_score_between_one_and_nine(char):
-        return int(char)
-    elif is_a_strike(char):
-        return 10
-    elif is_a_spare(char):
-        return 10
-    elif is_a_miss(char):
-        return 0
+def all_pins_down_in_the_first_roll(in_first_half, frame, current_roll_points):
+    if in_first_half:
+        in_first_half = False
     else:
-        raise ValueError()
+        frame += 1
+        in_first_half = True
+
+    if is_a_strike(current_roll_points):
+        in_first_half = True
+        frame += 1
+    return frame, in_first_half
 
 
 def is_a_score_between_one_and_nine(char):
@@ -70,3 +39,38 @@ def is_a_spare(char):
 
 def is_a_miss(char):
     return char == '-'
+
+
+def score(game):
+    result = 0
+    frame = 1
+    in_first_half = True
+    total_frames = 10
+    max_pins_number = 10
+
+    for roll_index in range(len(game)):
+        if is_a_spare(game[roll_index]):
+            result += total_frames - last_frame
+        else:
+            result += get_value(game[roll_index])
+
+        if frame < total_frames and get_value(game[roll_index]) == max_pins_number:
+            result = scoring_when_all_pins_down(game, result, frame, total_frames, max_pins_number, roll_index)
+
+        last_frame = get_value(game[roll_index])
+        frame, in_first_half = all_pins_down_in_the_first_roll(in_first_half, frame, game[roll_index])
+
+    return result
+
+
+def get_value(char):
+    if is_a_score_between_one_and_nine(char):
+        return int(char)
+    elif is_a_strike(char):
+        return 10
+    elif is_a_spare(char):
+        return 10
+    elif is_a_miss(char):
+        return 0
+    else:
+        raise ValueError()
