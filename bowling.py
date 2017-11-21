c8217dff864ae7f5d274a352aa4a42774dcc32ed
Orsolya Lévai <solya0082@gmail.com>
2017-11-21 18:56:25 +0100

    Removed after if conditional in_first_half == True

--- bowling.py
+++ bowling.py
@@ -19,7 +19,7 @@ def score(game):
         last = get_value(game[i])
         if not in_first_half:
             frame += 1
-        if in_first_half == True:
+        if in_first_half:
             in_first_half = False
         else:
             in_first_half = True
